05171155a6834d5bdb1fb01f5a9c1f63ff4ce229
Annetteskou <Eaa24amsk@students.eaaa.dk>
2025-01-26 21:41:03 +0100

    d

--- javascript-intro/js/js.js
+++ javascript-intro/js/js.js
@@ -2,4 +2,36 @@
  * file: js/js.js
  * purpose: Behaviors
  **/
-console.log('Success: JavaScript running!')
+console.log('Success: JavaScript running!');
+//console.log("Hello World");
+
+/** Her er en konsol, der skriver Mojn */ 
+//console.log("Mojn Do");// Her er en kommentar
+
+/**Variabler strenge */
+let a = "Huske at skrive til Michael i aften...";
+let b = "23";
+
+/** Variabler: tal - JavaScript kan regne*/
+let c = 23;
+console.log(b + b);
+console.log(c * c);
+
+/** Booleans: true / false */
+let julemandenEksisterer = true;
+console.log("Findes julemanden? " + julemandenEksisterer);
+
+let aftenmad = false; 
+
+let vaer_23_booked = false;
+
+/**Kontrolstrukturer */
+
+
+
+/**Konkatenering: "klistre sammen" med + */
+let header = "<h1>" + a + "</h1>";
+console.log(header);
+
+document.getElementById("hej").innerHTML = "header";
+document.getElementById("hej").style.color = "blue";
